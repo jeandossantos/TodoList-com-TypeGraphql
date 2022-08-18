50bc57c715f5617e0425c8eea9da3c8ef5e159bd
Jean dos Santos <jeanddg@hotmail.com>
2022-08-18 17:08:45 -0300

    class Recipe created

--- src/models/Recipe.ts
+++ src/models/Recipe.ts
@@ -1,19 +1,22 @@
 import { Field, ID, ObjectType } from 'type-graphql';
 
 @ObjectType()
-export class Recipe {
+export class User {
   @Field((type) => ID)
   id: string;
 
   @Field()
-  title: string;
-
-  @Field({ nullable: true })
-  description?: string;
+  name: string;
 
   @Field()
-  creationDate: Date;
+  initials: string;
 
-  @Field((type) => [String])
-  ingredients: string[];
+  @Field()
+  email: string;
+
+  @Field()
+  password: string;
+
+  @Field()
+  createdAt: Date;
 }
