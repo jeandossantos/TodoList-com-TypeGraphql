4c406d882498a61d93380939687afd58ff28ce0f
Jean dos Santos <jeanddg@hotmail.com>
2022-08-18 18:03:44 -0300

    resolver craeateUser created

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,4 +1,34 @@
-import { Resolver } from 'type-graphql';
+import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import { User } from '../models/User';
+import { prisma } from '../prisma';
+import { encryptPassword } from '../utils/helpers';
 
 @Resolver()
-export class RecipeResolver {}
+export class UserResolver {
+  @Query((returns) => String)
+  async hello() {
+    return 'Hello';
+  }
+
+  @Mutation((returns) => User)
+  async createUser(
+    @Arg('name') name: string,
+    @Arg('email') email: string,
+    @Arg('initials') initials: string,
+    @Arg('password') password: string,
+    @Arg('confirmPassword') confirmPassword: string
+  ) {
+    const encryptedPassword = encryptPassword(password);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        initials,
+        password: encryptedPassword,
+      },
+    });
+
+    return user;
+  }
+}
