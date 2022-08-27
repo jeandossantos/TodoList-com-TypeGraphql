c108338dab2d2bfa548d72bc7b5daa030e2f3047
Jean dos Santos <jeanddg@hotmail.com>
2022-08-27 16:48:23 -0300

    userResolver updated

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,11 +1,15 @@
-import { Arg, Args, Mutation, Query, Resolver } from 'type-graphql';
-import { User } from '../models/User';
+import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import { compareSync } from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 import { UserPayload } from '../models/UserPayload';
 import { UserRepository } from '../repositories/userRepository/UserRepository';
-
 import { encryptPassword } from '../utils/helpers';
-import { CreateUserArgs } from './args/CreateUserArgs';
-import { UpdateUserArgs } from './args/UpdateUserArgs';
+import { CreateUserInput } from '../inputs/CreateUserInput';
+import { UpdateUserInput } from '../inputs/UpdateUserInput';
+import { CustomException } from '../exceptions/CustomException';
+import { User } from '../models/User';
+import { equalsOrError } from '../utils/validators';
 
 const userRepository = new UserRepository();
 
@@ -17,13 +21,36 @@ export class UserResolver {
     @Arg('password') password: string
   ) {
     if (!email || !password) {
-      throw new Error('Enter E-mail and password!');
+      throw new CustomException('Enter E-mail and password!');
     }
+
+    const user = await userRepository.findByEmail(email);
+
+    if (!user) throw new CustomException('E-mail not registered!');
+
+    const isMatch = compareSync(password, user.password);
+
+    if (!isMatch) throw new CustomException('E-mail and/or password invalid.');
+
+    const payload = {
+      id: user.id,
+      email: user.email,
+      initials: user.initials,
+    };
+
+    const token = jwt.sign(payload, process.env.MY_SECRET, {
+      subject: user.id,
+    });
+
+    return { user, token };
   }
 
   @Mutation((returns) => User)
-  async createUser(@Args() createUserArgs: CreateUserArgs) {
-    const { name, email, initials, password, confirmPassword } = createUserArgs;
+  async createUser(@Arg('data') createUserInput: CreateUserInput) {
+    const { name, email, initials, password, confirmPassword } =
+      createUserInput;
+
+    equalsOrError(password, confirmPassword, 'Senhas não combinam!');
 
     const encryptedPassword = encryptPassword(password);
 
@@ -38,8 +65,8 @@ export class UserResolver {
   }
 
   @Mutation((returns) => User)
-  async updateUser(@Args() updateUserArgs: UpdateUserArgs) {
-    const { userId, initials, name } = updateUserArgs;
+  async updateUser(@Arg('data') updateUserInput: UpdateUserInput) {
+    const { userId, initials, name } = updateUserInput;
 
     const user = await userRepository.update(userId, name, initials);
 
